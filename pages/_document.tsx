6a892d77a2b7d3dfbf5ba03e4b4dbd5d8e989efd
ayushcompsci <ayushcompsci@gmail.com>
2022-08-21 15:57:31 -0500

    Base

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'normal']" lang="en">
+
+      <Head>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+
+        {/* import fonts */}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"/>
+
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+
+    </Html>
+  )
+}
